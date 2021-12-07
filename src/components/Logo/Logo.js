446bf8d14e8f95bc35e4c9f7cd2e5aa8d89a89c3
Divyansh Pandey <73097571+DivyanshPandey99@users.noreply.github.com>
2021-12-07 17:23:26 +0530

    Update Logo.js

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,14 +1,14 @@
 import React, {Component} from "react";
 import Tilt from 'react-tilt';
 import './Logo.css';
-import brain from './brain.png';
+import face from './facial-recognition.png';
 class Logo extends Component{
     render(){
         return (
             <div className="outDiv">
                 <Tilt className="Tilt" options={{ max : 55 }} style={{ height: 100, width: 100 }} >
                     <div className="Tilt-inner"> 
-                        <img style={{"paddingTop":"0"}} src = {brain} alt ="brain-logo"></img> 
+                        <img style={{"paddingTop":"0"}} src = {face} alt ="face-logo" width="100" height="100"></img> 
                     </div>
                 </Tilt>
             </div>
